3dbb667d4e87f01ef145673886c4391d7373defd
deveucatur <117214850+deveucatur@users.noreply.github.com>
2023-06-13 10:23:38 -0400

    Add files via upload

---
+++ Cadastro.py
@@ -0,0 +1,111 @@
+import streamlit as st
+import mysql.connector
+from PIL import Image
+import streamlit_authenticator as stauth
+
+
+conexao = mysql.connector.connect(
+    passwd='nineboxeucatur',
+    port=3306,
+    user='ninebox',
+    host='nineboxeucatur.c7rugjkck183.sa-east-1.rds.amazonaws.com',
+    database='Colaboradores'
+)
+
+mycursor = conexao.cursor()
+
+st.set_page_config(
+page_title="9box | Novo Cadastro",
+page_icon=Image.open('icon.png'),
+layout="centered")
+
+image = Image.open(('logo.png'))
+st.image(image, width = 250)
+comando2 = 'SELECT * FROM Usuarios;'
+mycursor.execute(comando2)
+listCod = mycursor.fetchall()
+
+
+def CadastroDeUsuarios():
+    with st.form("Formulário de Cadastro"):
+        st.subheader("Cadastro de Novos Usuários | 9box")
+        codCadas = st.text_input("Insira o código de cadastro de novo usuário recebido")
+        unidade = st.selectbox("Unidade de Negócio",  ["CEEM Ariquemes",
+                                                          "CEEM Boa Vista - Manaus",
+                                                          "CEEM Cacoal",
+                                                          "CEEM Campinas",
+                                                          "CEEM Campo Grande",
+                                                          "CEEM Cascavel",
+                                                          "CEEM Cuiaba",
+                                                          "CEEM Curitiba",
+                                                          "CEEM Goiânia",
+                                                          "CEEM Jí-Parana",
+                                                          "CEEM Porto Alegre",
+                                                          "CEEM Porto Velho",
+                                                          "CEEM Pres. Prudente",
+                                                          "CEEM Rio Branco",
+                                                          "CEEM Rondonópolis",
+                                                          "CEEM São Paulo",
+                                                          "CEEM Vilhena",
+                                                          "Corporativo Cascavel",
+                                                          "Corporativo Jí-Parana"])
+        col1, col2 = st.columns((2,0.8))
+        with col1:
+            nome = st.text_input("Nome")
+
+        with col2:
+            matricul = st.text_input('Matricula')
+        
+
+        email = st.text_input("Insira seu e-mail/Login")
+        col1, col2 = st.columns(2)
+        with col1:
+            senhaaux =st.text_input("Insira uma senha", type = "password")
+        with col2:
+            senha = st.text_input("Confirme a senha", type = "password")
+            if senha != senhaaux:
+                st.error("As senhas precisam ser iguais")
+        
+        
+        submitted = st.form_submit_button("Cadastrar")
+        if submitted and senha == senhaaux:
+            if codCadas in [x[1] for x in listCod]:
+                linhaBD = [x for x in range(len(listCod)) if  str(listCod[x][1]) == codCadas][0]
+
+                maiusculo = False
+
+                for string_email in email:
+                    if string_email.isupper():
+                        maiusculo = True
+
+                if maiusculo == False:
+                    if listCod[linhaBD][7] == '' or listCod[linhaBD][7] == None and email not in [x[7] for x in listCod] and matricul not in [x[4] for x in listCod]:
+                        #f"{nome,email,senha,stauth.Hasher(senha).generate()[0],1}"
+                        linrow = [unidade, matricul, nome, str(email).strip(), str(senha).strip(), stauth.Hasher([senha]).generate()[0]]
+                        colunas = ['Unid_Negocio', 'Matricula', 'Nome',
+                                        'Email', "senha", 'Senha_hashe']
+                        
+                        for i in range(len(linrow)):   
+                            sql = f"UPDATE Usuarios SET {colunas[i]} = '{linrow[i]}' WHERE cod_acesso = '{codCadas}'"
+                            mycursor.execute(sql)
+                            conexao.commit()
+                        
+                        st.success("Cadastro Realizado com Sucesso!")
+                        st.success("""Realize seu Login
+
+Com seu email e sua senha realize o login no seguinte link:
+
+---
+https://9box.eucatur.com.br/
+---""")
+
+                    else:
+                        st.error("Código de cadastro ou e-mail já foi utilizado")
+                else:
+                    st.error("O e-mail só pode conter letras minúsculas")
+            else:
+                st.error("Código de cadastro inválido")
+
+
+st.write("---")
+CadastroDeUsuarios()
---
+++ pages/1 - Recuperar Acessos.py
@@ -0,0 +1,63 @@
+import streamlit as st
+from PIL import Image
+import mysql.connector
+import streamlit_authenticator as stauth
+
+st.set_page_config(
+    page_title="Recuperar Acesso",
+    page_icon=Image.open('icon.png'),
+    layout="centered",
+)
+
+conexao = mysql.connector.connect(
+    passwd='nineboxeucatur',
+    port=3306,
+    user='ninebox',
+    host='nineboxeucatur.c7rugjkck183.sa-east-1.rds.amazonaws.com',
+    database='Colaboradores'
+)
+
+mycursor = conexao.cursor()
+comando2 = 'SELECT * FROM Usuarios;'
+mycursor.execute(comando2)
+dadosUser = mycursor.fetchall()
+
+
+listEmails = [x[7] for x in dadosUser]
+st.image(Image.open("logo.png"), width = 250)
+
+with st.form('Esqueceu a senha'):
+    st.subheader('Recupere seu acesso ao 9Box!')
+    cod_cadastr = st.text_input('Insira o código de cadastro')
+    matricul = st.text_input('Matricula')
+    new_email = st.text_input('Novo e-mail/login')
+    new_senha = st.text_input('Nova senha', type='password')
+
+    btao = st.form_submit_button('Alterar dados')
+
+    if btao:
+        new_senha_hashe = stauth.Hasher([str(new_senha).strip()]).generate()[0]
+        if new_email not in listEmails:
+            maiusculo = False
+
+            for string_email in new_email:
+                if string_email.isupper():
+                    maiusculo = True
+
+            if maiusculo == False:
+                linrow = [str(new_email).strip(), str(new_senha).strip(), new_senha_hashe]
+                colunas = ['Email', 'senha', 'Senha_hashe']
+                    
+                for i in range(len(colunas)):
+                    sql = f"UPDATE Usuarios SET {colunas[i]} = '{linrow[i]}' WHERE (cod_acesso = '{cod_cadastr}') AND (Matricula = {matricul});"
+                    mycursor.execute(sql)
+                    conexao.commit()
+
+                st.info('''Dados de login alterados com sucesso
+                    
+Basta fazer o [login](https://9box.eucatur.com.br/) na tela principal do sistema 9Box.''') 
+            else:
+                st.error("O e-mail só pode conter letras minúsculas.")
+        else:
+            st.error("E-mail já utilizado por outro usuário.")
+            
